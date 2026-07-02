0df16b00157b4a0f1da324b5e838d226ee514814
freelaunching123 <freelaunching123@gmail.com>
2026-07-02 21:26:09 +0530

    Delete mobile directory

--- mobile/android/app/src/main/kotlin/com/healthyhomefoods/healthy_home_foods/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.healthyhomefoods.healthy_home_foods
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- mobile/lib/core/utils/file_saver.dart
+++
@@ -1,1 +0,0 @@
-export 'file_saver_mobile.dart' if (dart.library.html) 'file_saver_web.dart';
