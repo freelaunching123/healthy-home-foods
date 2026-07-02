da8edd3585b8023bd2761be9e864b49c7a14cdfb
freelaunching123 <freelaunching123@gmail.com>
2026-07-02 21:25:16 +0530

    Delete backend directory

--- backend/check_products.py
+++
@@ -1,8 +0,0 @@
-import os
-from sqlalchemy import create_engine, text
-
-engine = create_engine(os.environ['SYNC_DATABASE_URL'])
-with engine.connect() as conn:
-    res = conn.execute(text("SELECT id, name, created_at FROM products ORDER BY created_at DESC LIMIT 5"))
-    for row in res:
-        print(row)
